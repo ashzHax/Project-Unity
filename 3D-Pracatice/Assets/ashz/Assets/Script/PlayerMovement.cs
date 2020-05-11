e0cd875411958cb1a4ad0160f290939369ce6823
Ash <ashkim2001@gmail.com>
2020-05-11 23:55:54 +0900

    ashz upload

--- 3D-Pracatice/Assets/ashz/Assets/Script/PlayerMovement.cs
+++ 3D-Pracatice/Assets/ashz/Assets/Script/PlayerMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using UnityEngine;
 
@@ -8,6 +9,8 @@ namespace ashz
     public class PlayerMovement : MonoBehaviour
     {
         public Rigidbody rb;
+        public float forwardForce = 2000f;
+        public float sidewaysForce = 500f;
 
         // Start is called before the first frame update
         /*
@@ -29,7 +32,15 @@ namespace ashz
         // FixedUpdate is prefered when changing physics (more smoother)
         void FixedUpdate()
         {
-            rb.AddForce(0, 0, 2000 * Time.deltaTime);
+            rb.AddForce(/*X*/0, /*Y*/0, /*Z*/forwardForce * Time.deltaTime);
+            if ( Input.GetKey("d") )
+            {
+                rb.AddForce( sidewaysForce * Time.deltaTime, 0, 0);
+            }
+            if ( Input.GetKey("a") )
+            {
+                rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            }
         }
     }
 }
