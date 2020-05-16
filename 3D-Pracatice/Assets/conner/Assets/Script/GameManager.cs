5572adf099869ce8a2987d3f3cf8c91f3f849c25
ryumj0117 <45999965+ryumj0117@users.noreply.github.com>
2020-05-16 18:18:50 +0900

    final result

--- 3D-Pracatice/Assets/conner/Assets/Script/GameManager.cs
+++ 3D-Pracatice/Assets/conner/Assets/Script/GameManager.cs
@@ -7,6 +7,19 @@ public class GameManager : MonoBehaviour
     bool gameEnded = false;
     public float restartDelay = 3f;
 
+    public GameObject CompleteLevelUI;
+
+    void Start()
+    {
+        CompleteLevelUI.SetActive(false);
+    }
+
+    public void CompleteLevel()
+    {
+        CompleteLevelUI.SetActive(true);
+        Debug.Log("LEVEL COMPLETE");
+    }
+
     public void GameOver()
     {
         if (gameEnded == false)
