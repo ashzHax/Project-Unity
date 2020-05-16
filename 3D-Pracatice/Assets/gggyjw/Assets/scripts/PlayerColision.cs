d61490cba196018f9dcd768b5dd3cd261a9a1662
gggyjw <57584910+gggyjw@users.noreply.github.com>
2020-05-16 23:44:33 +0900

    broken code upload

---
+++ 3D-Pracatice/Assets/gggyjw/Assets/scripts/PlayerColision.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using gggyjw;
+
+namespace gggyjw
+{
+    
+    public class PlayerColision : MonoBehaviour
+    {
+        public PlayerMovement movement;
+
+        void OnCollisionEnter(Collision collisionInfo)
+        {
+            if (collisionInfo.collider.name == "obstacle")
+            {
+                movement.enabled = false;
+                FindObjectOfType<GameManager>().EndGame();
+                
+            }
+        }
+    }
+}
