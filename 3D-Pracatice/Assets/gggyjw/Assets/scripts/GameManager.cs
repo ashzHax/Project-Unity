f019810ae1081a14f579cf5971d5086e4f8b9d59
gggyjw <57584910+gggyjw@users.noreply.github.com>
2020-05-19 21:08:44 +0900

    hi

--- 3D-Pracatice/Assets/gggyjw/Assets/scripts/GameManager.cs
+++ 3D-Pracatice/Assets/gggyjw/Assets/scripts/GameManager.cs
@@ -1,14 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace gggyjw
 {
     public class GameManager : MonoBehaviour
     {
+        bool gameHasEnded = false;
+
+        public float restartDelay = 1f;
+
+        public GameObject completeLevelUI;
+
+        public void completeLevel1()
+        {
+            completeLevelUI.SetActive(true);
+        }
+        
+        public void CompleteLevel1()
+        {
+            completeLevelUI.SetActive(true);
+        }
         public void EndGame()
         {
-            Debug.Log("Game Over");
+            if(gameHasEnded == false)
+            {
+                gameHasEnded = true;
+                Debug.Log("Game Over");
+                Invoke("Restart", restartDelay);
+            }
+        }
+
+        void Restart ()
+        {
+             SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
         }
     }
 }
