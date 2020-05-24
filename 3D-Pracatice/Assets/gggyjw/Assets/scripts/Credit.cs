389330f8b87a21edf186e9ed1260674f4c6dc834
gggyjw <57584910+gggyjw@users.noreply.github.com>
2020-05-24 18:16:32 +0900

    finished

---
+++ 3D-Pracatice/Assets/gggyjw/Assets/scripts/Credit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace gggyjw
+{
+    public class Credit : MonoBehaviour
+    {           
+        
+        public void Quit()
+        {
+            Debug.Log("Quit");
+            Application.Quit();
+        }
+    }
+}
