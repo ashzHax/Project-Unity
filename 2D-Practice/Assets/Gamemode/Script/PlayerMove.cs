47b7429c6d300828b3afdc5b81bdb1015306ca3e
Gamemode1221 <50878455+Gamemode1221@users.noreply.github.com>
2020-06-06 21:13:48 +0900

    Temp Commit

---
+++ 2D-Practice/Assets/Gamemode/Script/PlayerMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gamemode;
+using System.Threading;
+
+namespace Gamemode
+{
+    public class PlayerMove : MonoBehaviour
+    {
+
+        public CharacterController controller;
+
+        public float runSpeed = 40f;
+
+        float horizontalMove = 0f;
+        bool jump = false;
+        bool crouch = false;
+
+        // Update is called once per frame
+        void Update()
+        {
+            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                jump = true;
+            }
+
+            if (Input.GetButtonDown("Crouch"))
+            {
+                crouch = true;
+            }
+        }
+
+        void FixedUpdate()
+        {
+            controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+            jump = false;
+        }
+    }
+
+}
