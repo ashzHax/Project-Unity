add3772e833c8f83461c1c4b0569e0bcea8f444d
gggyjw <57584910+gggyjw@users.noreply.github.com>
2020-06-14 17:18:43 +0900

    can't jump

---
+++ 2D-Practice/Assets/gggyjw/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace gggyjw
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+
+        public CharacterController2D controller;
+
+        public float runSpeed = 40f;
+        
+        bool Jump = false;
+
+        float horizontalMove = 0f;
+
+        void Update()
+        {
+            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                Jump = true;
+            }
+        }
+
+         void FixedUpdate()
+        {
+            controller.Move(horizontalMove * Time.fixedDeltaTime, false, Jump);
+            Jump = false;
+
+        }
+    }
+}
